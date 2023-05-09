970b41cba15ab3dfc41fdabe3f208af8c767e4cf
Shekhar <droidmac07@gmail.com>
2023-05-09 14:47:36 +0530

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
-import 'package:catalog1/home_page.dart';
+import 'package:catalog1/login_page.dart';
+import 'package:catalog1/page/home_page.dart';
 import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override Widget build(BuildContext context) {
-    return const MaterialApp(
-      home:HomePage()
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        // home:const HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/":(context)=>LoginPage() ,
+        "/login": (context)=> LoginPage(),
+        "/home": (context)=> HomePage(),
+
+
+      }
     );
   }
 }
